d5b13cc84c908decc6c2b11bf41095c8c6eba6cf
dev <developerwhitty@gmail.com>
2023-10-15 18:05:47 -0230

    update

--- Source Code/drone.cs
+++ Source Code/drone.cs
@@ -1,35 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+using GorillaLocomotion.Climbing;
 using UnityEngine;
 
-public class drone : MonoBehaviour
+public class Drone : MonoBehaviour
 {
-    public bool on = false;
+    public static Drone Instance { get; private set; }
+
+    public bool on;
+
+    private float touchTime = 0f;
+    private const float debounceTime = 0.7f;
+
+    public void Awake() => Instance ??= this;
+
     // Start is called before the first frame update
     private void OnTriggerEnter(Collider other)
     {
+        if (touchTime + debounceTime >= Time.time) return;
 
-        clicked(on);
-    }
-    public void clicked(bool ison)
-    {
-        if (!ison)
+        if (other.TryGetComponent(out GorillaTriggerColliderHandIndicator indicator))
         {
-            on = true;
-        }
-        if (ison)
-        {
-            on = false;
+            touchTime = Time.time;
+
+            GorillaTagger.Instance.offlineVRRig.PlayHandTapLocal(67, indicator.isLeftHand, 0.05f);
+            GorillaTagger.Instance.StartVibration(indicator.isLeftHand, GorillaTagger.Instance.tapHapticStrength / 2f, GorillaTagger.Instance.tapHapticDuration);
+
+            clicked(on);
         }
     }
 
-
+    public void clicked(bool ison) => on = !ison;
 
     // Update is called once per frame
     void Update()
     {
-
-
         if (on)
         {
 
