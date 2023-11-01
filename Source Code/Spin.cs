e8eb2b5cd417bcebcceaade70ba79b46e0d0cc67
striker67 <86896895+Striker-67@users.noreply.github.com>
2023-11-01 16:58:55 -0500

    Add files via upload

---
+++ Source Code/Spin.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace DroneMod
+{
+
+    public class Spin : MonoBehaviour
+    {
+        float speed;
+
+        public void FixedUpdate()
+        {
+            if (!Plugin.inRoom) return;
+            speed = Mathf.Lerp(speed, Drone.Instance.on ? 400f : 0f, 3f * Time.deltaTime);
+
+            transform.Rotate(0, 0, speed);
+        }
+    }
+}
